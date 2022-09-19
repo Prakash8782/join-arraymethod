00afd797f0e2b66f77b692c4109fbd60a9f9956d
Prakash8782 <prakash8782@gmail.com>
2022-09-19 15:18:06 +0530

    join

---
+++ join/main.js
@@ -0,0 +1,7 @@
+let colors=["Red","Blue","Green","Yellow","Pink"];
+console.log("colors : " ,colors);
+console.log("length of an array : "+colors.length);
+console.log("method : array.join()");
+let a=colors.join(" and ");
+console.log("change in array : ",a);
+console.log("return : length of array after join(); is "+colors.length);
